c29974c570d6a4a827613bf0a5055787f286160b
Gaurang2737 <gaurangsane9@gmail.com>
2025-07-12 01:05:21 +0530

    fix: replace wouter with react-router-dom in Navbar

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "wouter";
+import { Link } from "react-router-dom";
 import { Menu, X } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
